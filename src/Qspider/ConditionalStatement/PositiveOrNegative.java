8e9182cce976a2be6bdef86618dcec39105158a4
swati yadav <swatiyadav6872@gmail.com>
2024-12-28 22:57:52 +0530

    init commit

---
+++ src/Qspider/ConditionalStatement/PositiveOrNegative.java
@@ -0,0 +1,13 @@
+package Qspider.ConditionalStatement;
+
+public class PositiveOrNegative {
+    public static void main(String[] args) {
+        int num=-45;
+        if (num>=0){
+            System.out.println("Number is positive");
+        }
+        else {
+            System.out.println("Number is negative");
+        }
+    }
+}
