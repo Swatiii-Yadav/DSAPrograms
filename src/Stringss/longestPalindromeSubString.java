37770363a2f4373a78cd4c9870b4263d7ec1e193
swati yadav <swatiyadav6872@gmail.com>
2025-01-14 09:51:29 +0530

    init commit

--- src/Stringss/longestPalindromeSubString.java
+++ src/Stringss/longestPalindromeSubString.java
@@ -1,36 +1,40 @@
 package Stringss;
 
 public class longestPalindromeSubString {
-        public static String longestPalindrome(String s) {
-            if (s == null || s.length() < 1) {
-                return "";
-            }
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
 
-            int start = 0, end = 0;
-
+        String longest = "";
         for (int i = 0; i < s.length(); i++) {
-                int len1 = palindrome(s, i, i);
-                int len2 = palindrome(s, i, i + 1);
+            for (int j = i; j < s.length(); j++) {
 
-                int result = Math.max(len1, len2);
+                String substring = s.substring(i, j + 1);
 
-                if (result > end - start) {
-                    start = i - (result - 1) / 2;
-                    end = i + result / 2;
+
+                if (isPalindrome(substring)) {
+                    if (substring.length() > longest.length()) {
+                        longest = substring;
+                    }
                 }
             }
-
-            return s.substring(start, end + 1);
         }
-        private static int palindrome(String s, int left, int right) {
- while (left >= 0 && right < s.length() &&
-                    s.charAt(left) == s.charAt(right)) {
-                left--;
-                right++;
+
+        return longest;
+    }
+
+    private static boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
             }
-            return right - left - 1;
+            left++;
+            right--;
         }
-
+        return true;
+    }
         public static void main(String[] args) {
             String input = "babad";
             String result = longestPalindrome(input);
