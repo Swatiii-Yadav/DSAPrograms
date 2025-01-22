37e7028339126ed69e23e534ea9ebe6b016b5371
swati yadav <swatiyadav6872@gmail.com>
2025-01-22 23:41:08 +0530

    init commit

---
+++ src/january22/hollowDiamond.java
@@ -0,0 +1,38 @@
+package january22;
+
+public class hollowDiamond {
+    public static void main(String[] args) {
+                int n = 5;
+                for (int i = 1; i <= n; i++) {
+
+                    for (int j = 1; j <= n - i; j++) {
+                        System.out.print(" ");
+                    }
+                    System.out.print("*");
+                    if (i > 1) {
+                        for (int j = 1; j <= 2 * i - 3; j++) {
+                            System.out.print(" ");
+                        }
+                        System.out.print("*");
+                    }
+                    System.out.println();
+                }
+        for (int i = n; i>=0; i--) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            if (i > 1) {
+                for (int j = 1; j <= 2 * i - 3; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+            }
+        }
+
