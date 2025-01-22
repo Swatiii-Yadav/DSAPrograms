b957ce8d24307eb9becab82b3960d16328e4d9af
swati yadav <swatiyadav6872@gmail.com>
2025-01-22 21:23:36 +0530

    init commit

---
+++ src/Patterns/DownWardPyramid.java
@@ -0,0 +1,14 @@
+package Patterns;
+
+public class DownWardPyramid {
+    public static void main(String[] args) {
+        int n=4;
+        for (int i=0;i<n;i++){
+            for (int j=i;j<n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
