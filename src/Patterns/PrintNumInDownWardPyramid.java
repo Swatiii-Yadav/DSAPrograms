fb9475c48c827b2cf4d06a780887ba3b9e07d11c
swati yadav <swatiyadav6872@gmail.com>
2025-01-22 21:24:38 +0530

    init commit

---
+++ src/Patterns/PrintNumInDownWardPyramid.java
@@ -0,0 +1,14 @@
+package Patterns;
+
+public class PrintNumInDownWardPyramid {
+    public static void main(String[] args) {
+        int n=6;
+        for (int i=1;i<=n;i++){
+            for (int j=i;j<=n;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+    }
+}
