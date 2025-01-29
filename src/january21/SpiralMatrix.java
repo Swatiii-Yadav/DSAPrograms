8b0abdab973d23e4738877cdc9d7ad724a9ab156
swati yadav <swatiyadav6872@gmail.com>
2025-01-29 22:41:55 +0530

    init commit

---
+++ src/january21/SpiralMatrix.java
@@ -0,0 +1,18 @@
+package january21;
+
+import java.util.Scanner;
+
+public class SpiralMatrix {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+     int row=sc.nextInt();
+        int col=sc.nextInt();
+        int  [][] matrix= new int[][]{{1, 2},{3,4}};
+        for (int i=0;i<row;i++){
+            for (int j=0;j<col;j++){
+
+                System.out.print(matrix[i][j]+" ");
+            }
+        }
+    }
+}
